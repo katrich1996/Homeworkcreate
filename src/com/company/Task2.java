1ecddcb8d18fbb5501b36aee248228974838be1a
katrich1996 <sergey.katrich96@mail.ru>
2022-03-16 21:42:41 +0300

    Test

---
+++ src/com/company/Task2.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+    int clientOS = 0;
+    int clientDeviceYear = 2015;
+    if (clientOS == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке");
+    }
+    if (clientOS == 0) {
+        System.out.println("Установите версию приложения для IOS по ссылке");
+    }
+    else if (clientDeviceYear <= 2014 && clientOS == 1) {
+        System.out.println("Установите облегченную версию приложения для Android по ссылке");
+    }
+}
+
+}
+
